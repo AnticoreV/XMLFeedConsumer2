0243281335830f1499b99a53957a22fa98fddebd
AnticoreV <sapronovivancore@gmail.com>
2022-11-16 17:36:53 +0100

    Refactored 0.2

--- src/main/java/com/XMLFeedConsumer2/xml/StAXParser.java
+++ src/main/java/com/XMLFeedConsumer2/xml/StAXParser.java
@@ -17,10 +17,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-
-//Refactoring in process
 @Service
 public class StAXParser {
     private static final Logger log = LoggerFactory.getLogger(StAXParser.class);
@@ -38,47 +38,31 @@ public class StAXParser {
     @Bean
     public void parse() {
         try {
-            Reader fileReader = new FileReader(path);
-            XMLInputFactory xmlInputFactory =
-                    XMLInputFactory.newInstance();
-            XMLStreamReader xmlStreamReader =
-                    xmlInputFactory.createXMLStreamReader(fileReader);
-
-            //test
-
-
-            List<String> product_names = new ArrayList<>();
-            List<String> info = new ArrayList<>();
-            List<String> countries = new ArrayList<>();
-            List<String> param = new ArrayList<>();
+            XMLStreamReader xmlStreamReader = getXmlStreamReader();
+            Map<String, List<String>> map = updateMap();
             int iter = 0;
             int param_iter = 0;
             Description description = new Description();
             Product product = new Product();
-
             while (xmlStreamReader.hasNext()) {
                 //Get integer value of current event
                 xmlStreamReader.next();
-
                 if(xmlStreamReader.isStartElement()){
                     if(isElement(xmlStreamReader, "Product")){
                         iter++;
                     } else if (isElement(xmlStreamReader, "ID")) {
                         idElementProcessing(xmlStreamReader, product);
                     } else if (isElement(xmlStreamReader, "Name")) {
-                        nameElementProcessing(xmlStreamReader, product_names);
+                        nameElementProcessing(xmlStreamReader, map.get("product_names"));
                     } else if (isElement(xmlStreamReader, "PartNumber")) {
                         partNumberElementProcessing(xmlStreamReader, product);
                     } else if (isElement(xmlStreamReader, "Country")) {
-                        countryElementProcessing(xmlStreamReader, countries);
+                        countryElementProcessing(xmlStreamReader, map.get("countries"));
                     } else if (isElement(xmlStreamReader, "Param")) {
                         param_iter++;
                     }
                 }else if (xmlStreamReader.hasText() && xmlStreamReader.getText().trim().length() > 0){
-                    if(param_iter % 2 != 0){
-                        param.add(xmlStreamReader.getText());
-                        param_iter = 0;
-                    }
+                    param_iter = getParam_iter(xmlStreamReader, map.get("param"), param_iter);
                 }
                 else if(xmlStreamReader.isEndElement()){
                     if(isElement(xmlStreamReader, "Product")){
@@ -90,17 +74,9 @@ public class StAXParser {
                 // => iter == 2, than we save an object and create new dataset
                 //for the next product
                 if((iter%2)==0 && iter!=0){
-                    description.setOther_info(info.toString());
-                    description.setParam(param.toString());
-                    product.setNames(product_names.toString());
-                    product.setCountries(countries.toString());
-                    productService.save(product);
-                    descriptionService.save(description);
+                    saveProductToDb(map.get("product_names"), map.get("info"), map.get("countries"), map.get("param"), description, product);
                     log.info("Product was added " + product.getProduct_id());
-                    countries = new ArrayList<>();
-                    product_names = new ArrayList<>();
-                    info = new ArrayList<>();
-                    param = new ArrayList<>();
+                    map = updateMap();
                     product = new Product();
                     description = new Description();
                     iter = 0;
@@ -109,6 +85,42 @@ public class StAXParser {
         } catch (XMLStreamException | FileNotFoundException e) {
             throw new RuntimeException("Runtime Exception " + e);
         }
+    }
+
+    private XMLStreamReader getXmlStreamReader() throws FileNotFoundException, XMLStreamException {
+        Reader fileReader = new FileReader(path);
+        XMLInputFactory xmlInputFactory =
+                XMLInputFactory.newInstance();
+        XMLStreamReader xmlStreamReader =
+                xmlInputFactory.createXMLStreamReader(fileReader);
+        return xmlStreamReader;
+    }
+
+    private Map<String, List<String>> updateMap() {
+        Map<String, List<String>> map;
+        map = new HashMap<>();
+        map.put("product_names", new ArrayList<>());
+        map.put("info", new ArrayList<>());
+        map.put("countries", new ArrayList<>());
+        map.put("param", new ArrayList<>());
+        return map;
+    }
+
+    private void saveProductToDb(List<String> product_names, List<String> info, List<String> countries, List<String> param, Description description, Product product) {
+        description.setOther_info(info.toString());
+        description.setParam(param.toString());
+        product.setNames(product_names.toString());
+        product.setCountries(countries.toString());
+        productService.save(product);
+        descriptionService.save(description);
+    }
+
+    private int getParam_iter(XMLStreamReader xmlStreamReader, List<String> param, int param_iter) {
+        if(param_iter % 2 != 0){
+            param.add(xmlStreamReader.getText());
+            param_iter = 0;
+        }
+        return param_iter;
     }
 
     private void idElementProcessing(XMLStreamReader xmlStreamReader, Product product) throws XMLStreamException {
